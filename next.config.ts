5830a1adf3ef0cda515c18f09893fb41662b6dbc
SmitParekh84 <smartparekh02@gmail.com>
2026-05-01 10:07:52 +0530

    feat: update Content Security Policy to include additional image sources

--- next.config.ts
+++ next.config.ts
@@ -15,10 +15,10 @@ const securityHeaders = [
     key: "Content-Security-Policy",
     value: [
       "default-src 'self'",
-      "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
+      "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://platform.linkedin.com",
       "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
       "font-src 'self' https://fonts.gstatic.com",
-      "img-src 'self' data: blob: https://res.cloudinary.com https://placehold.co https://api.smitparekh.co.in http://localhost:5000 https://img.youtube.com",
+      "img-src 'self' data: blob: https://res.cloudinary.com https://placehold.co https://api.smitparekh.co.in http://localhost:5000 https://img.youtube.com https://media.licdn.com https://platform.linkedin.com",
       "connect-src 'self' https://www.smitparekh.co.in https://api.smitparekh.co.in https://*.supabase.co wss://*.supabase.co https://*.hf.space http://localhost:5000",
       "frame-ancestors 'self'",
     ].join("; "),
