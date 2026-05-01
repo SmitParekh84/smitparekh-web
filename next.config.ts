dd477a70e0d0af071a071fcf6ddcada2d984df07
SmitParekh84 <smartparekh02@gmail.com>
2026-05-01 10:09:24 +0530

    feat: update Content Security Policy to include additional image source for LinkedIn

--- next.config.ts
+++ next.config.ts
@@ -18,7 +18,7 @@ const securityHeaders = [
       "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://platform.linkedin.com",
       "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
       "font-src 'self' https://fonts.gstatic.com",
-      "img-src 'self' data: blob: https://res.cloudinary.com https://placehold.co https://api.smitparekh.co.in http://localhost:5000 https://img.youtube.com https://media.licdn.com https://platform.linkedin.com",
+      "img-src 'self' data: blob: https://res.cloudinary.com https://placehold.co https://api.smitparekh.co.in http://localhost:5000 https://img.youtube.com https://media.licdn.com https://dms.licdn.com https://platform.linkedin.com",
       "connect-src 'self' https://www.smitparekh.co.in https://api.smitparekh.co.in https://*.supabase.co wss://*.supabase.co https://*.hf.space http://localhost:5000",
       "frame-ancestors 'self'",
     ].join("; "),
